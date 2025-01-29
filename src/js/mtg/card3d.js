063595ad2bf1659f4caba09a51c6a8f298e155ce
Blaine Harper <bhar2254@gmail.com>
2025-01-29 13:07:38 -0600

    Update card3d.js

--- src/js/mtg/card3d.js
+++ src/js/mtg/card3d.js
@@ -1,30 +1,11 @@
-class Card3D {
-  constructor(element) {
-    this.element = element;
-    this.inner = element.querySelector(".card3d-inner");
-    this.image = element.querySelector("img");
-    this.init();
-  }
 
-  init() {
-    this.element.style.perspective = "1000px";
-    this.inner.style.transformStyle = "preserve-3d";
-    this.inner.style.transition = "transform 0.2s ease-out";
-    this.element.addEventListener("mousemove", this.handleMouseMove.bind(this));
-    this.element.addEventListener("mouseleave", this.handleMouseLeave.bind(this));
-  }
-
-  handleMouseMove(event) {
-    const { width, height, left, top } = this.element.getBoundingClientRect();
-    const x = (event.clientX - left - width / 2) / width * 2;
-    const y = (event.clientY - top - height / 2) / height * -2;
-
-    this.inner.style.transform = `rotateY(${x * 15}deg) rotateX(${y * 15}deg) scale(1.1)`;
-  }
-
-  handleMouseLeave() {
-    this.inner.style.transform = "rotateY(0) rotateX(0) scale(1)";
-  }
-}
-
-document.querySelectorAll(".card3d").forEach(card => new Card3D(card));
+// JavaScript for card click to focus
+document.querySelectorAll('.card3d').forEach(card => {
+  card.addEventListener('click', function() {
+    // Remove focus from any previously focused card
+    document.querySelectorAll('.card3d').forEach(c => c.classList.remove('focused'));
+    
+    // Add 'focused' class to the clicked card
+    this.classList.add('focused');
+  });
+});
